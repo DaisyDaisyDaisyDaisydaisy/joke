2f587daab083dcbe64c30efde82383fda1230753
LCX666 <chegnxiang@163.com>
2020-03-17 16:16:53 +0800

    由于几个分析的项目太大，不利于我们前期克隆和上传，我将其移除了，在需要的时候，可以使用其他方式进行传输

--- 工具集合/ShortestPathCUDA-master/src/ShortestPath/ShortestPath/macros.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MACROS_H_
-#define _MACROS_H_
-
-#define ERR(source) (fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
-                     perror(source),\
-		     		     exit(EXIT_FAILURE))
-#endif
